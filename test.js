a8e4ba10f56f531351f7a8c818b65fc998dce754
Alex A. Naanou <alex.nanou@gmail.com>
2025-12-26 19:31:54 +0300

    ...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- test.js
+++ test.js
@@ -1082,4 +1082,4 @@ typeof(__filename) != 'undefined'
 
 
 /**********************************************************************
-* vim:set ts=4 sw=4 :                               */ return module })
+* vim:set ts=4 sw=4 nowrap :                        */ return module })
